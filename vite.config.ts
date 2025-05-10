d2a76f21309f01cdd58494b353222acad91004b0
Václav Vančura <commit@vancura.dev>
2025-05-10 15:56:27 +0200

    feat: add hot reload
    Signed-off-by: Václav Vančura <commit@vancura.dev>

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
+  server: {
+    fs: {
+      allow: [
+        // Default allow list
+        ".",
+        // Allow access to Yarn PnP cache
+        ".yarn",
+        ".yarn/__virtual__",
+      ],
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
 });
