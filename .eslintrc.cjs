2f318d6b45241ff60406516cda7dd207b07ecc8e
Václav Vančura <commit@vancura.dev>
2025-06-01 08:55:23 +0200

    chore: configure editor and linting settings
    - Update .editorconfig with consistent formatting rules
    - Configure ESLint for modern JavaScript/TypeScript
    - Update .gitattributes for proper line endings
    - Add .cursorrules for AI assistant guidance
    - Add cspell.json for spell checking configuration
    
    Signed-off-by: Václav Vančura <commit@vancura.dev>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,29 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:svelte/recommended",
-    "prettier",
-  ],
-  plugins: ["@typescript-eslint"],
-  ignorePatterns: ["*.cjs"],
-  overrides: [
-    {
-      files: ["*.svelte"],
-      parser: "svelte-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-      },
-      rules: {
-        // Disable rules that have issues with Svelte files
-        "no-undef": "off",
-      },
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended', 'prettier'],
+    plugins: ['@typescript-eslint'],
+    ignorePatterns: ['*.cjs'],
+    overrides: [
+        {
+            files: ['*.svelte'],
+            parser: 'svelte-eslint-parser',
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+            },
+            rules: {
+                // Disable rules that have issues with Svelte files
+                'no-undef': 'off',
+            },
+        },
+    ],
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2020,
     },
-  ],
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2020,
-  },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true,
-  },
+    env: {
+        browser: true,
+        es2017: true,
+        node: true,
+    },
 };
